ad96ac37082ee527ac9899def25a7a33c17f6425
Sara L Pulit <sara.pulit@gmail.com>
2019-06-23 13:41:31 -0700

    Update sample_QC.sh

--- gwas/sample_QC.sh
+++ gwas/sample_QC.sh
@@ -29,14 +29,18 @@ for chr in {1..23}; do
 done
 
 ## SNPs to drop from super set -- lactase gene
-awk ' $1==2 && $4 > 129883530 && $4 < 140283530 { print $2 } ' $data/chr2.imputed.poly.subset300.bim > ./frayling.imputed.poly.subset300.pca.EXCLUDE.snps
+awk ' $1==2 && $4 > 129883530 && $4 < 140283530 { print $2 } ' $data/chr2.imputed.poly.subset300.bim > \
+	./frayling.imputed.poly.subset300.pca.EXCLUDE.snps
 
 ## Drop all of the SNPs in the major histocompatibility complex (hg19 coordinates)
-awk ' $1==6 && $4 > 24092021 && $4 < 38892022 { print $2 } ' $data/chr6.imputed.poly.subset300.bim >> ./frayling.imputed.poly.subset300.pca.EXCLUDE.snps
+awk ' $1==6 && $4 > 24092021 && $4 < 38892022 { print $2 } ' $data/chr6.imputed.poly.subset300.bim >> \
+	./frayling.imputed.poly.subset300.pca.EXCLUDE.snps
 
 ## Drop all of the SNPs in the inverted regions on chromosomes 8 and 17
-awk ' $1==8 && $4 > 6612592 && $4 < 13455629 { print $2 } ' $data/chr8.imputed.poly.subset300.bim >> ./frayling.imputed.poly.subset300.pca.EXCLUDE.snps
-awk ' $1==17 && $4 > 40546474 && $4 < 44644684 { print $2 } ' $data/chr17.imputed.poly.subset300.bim >> ./frayling.imputed.poly.subset300.pca.EXCLUDE.snps
+awk ' $1==8 && $4 > 6612592 && $4 < 13455629 { print $2 } ' $data/chr8.imputed.poly.subset300.bim >> \
+	./frayling.imputed.poly.subset300.pca.EXCLUDE.snps
+awk ' $1==17 && $4 > 40546474 && $4 < 44644684 { print $2 } ' $data/chr17.imputed.poly.subset300.bim >> \
+	./frayling.imputed.poly.subset300.pca.EXCLUDE.snps
 
 ## Extract the super set while removing the above SNPs; autosome only
 for chr in {1..22}; do
@@ -57,16 +61,31 @@ $plink --bfile chr1.imputed.poly.subset300.pca --merge-list frayling.imputed.pol
 
 ## remove inconsistent alleles (and repeat merge)
 for chr in {1..22}; do
-  $plink --bfile chr$chr.imputed.poly.subset300.pca --exclude merged.imputed.poly.subset300.pca-merge.missnp --allow-no-sex --make-bed --out chr$chr.imputed.poly.subset300.pca
+  $plink --bfile chr$chr.imputed.poly.subset300.pca --exclude merged.imputed.poly.subset300.pca-merge.missnp \
+  	--allow-no-sex \
+	--make-bed \
+	--out chr$chr.imputed.poly.subset300.pca
 done
 
 ## inbreeding
-$plink --bfile merged.imputed.poly.subset300.pca --allow-no-sex --het --out merged.imputed.poly.subset300.pca
+$plink --bfile merged.imputed.poly.subset300.pca \
+	--allow-no-sex \
+	--het \
+	--out merged.imputed.poly.subset300.pca
 
 ## relatedness
-$plink --bfile merged.imputed.poly.subset300.pca --allow-no-sex --genome --out merged.imputed.poly.subset300.pca
+$plink --bfile merged.imputed.poly.subset300.pca \
+	--allow-no-sex \
+	--genome \
+	--out merged.imputed.poly.subset300.pca
 
 ## run sex check
-$plink --bfile $data/chr23.imputed.poly.subset300 --update-sex fATDIVA_BMI_Data_Pairs.08.11.17.sex.txt \
-    --make-bed --out $home/chr23.imputed.poly.subset300
-$plink --bfile $home/chr23.imputed.poly.subset300 --check-sex --allow-no-sex --out $home/chr23.imputed.poly.subset300
+$plink --bfile $data/chr23.imputed.poly.subset300 \
+	--update-sex fATDIVA_BMI_Data_Pairs.08.11.17.sex.txt \
+   	--make-bed \
+    	--out $home/chr23.imputed.poly.subset300
+
+$plink --bfile $home/chr23.imputed.poly.subset300 \
+	--check-sex \
+	--allow-no-sex \
+	--out $home/chr23.imputed.poly.subset300
